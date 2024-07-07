f402ea79ba5e4513e37be32f874bf0dec9b20cda
iwan <=>
2024-07-08 00:47:55 +0300

    Доработка

--- fire_hazard/script.js
+++ fire_hazard/script.js
@@ -7,3 +7,18 @@ function showTooltip(event, id) {
     const tooltip = document.getElementById(id);
     tooltip.style.display = 'block';
 }
+
+function changeText() {
+    if (window.innerWidth <= 450) {
+        document.querySelector('#tooltip1').html = '<strong>Опасность!</strong> Открытое окно может опрокинуть горящую свечу и вызвать пожар.';
+        document.querySelector('#tooltip2').html = '<strong>Опасность!</strong> Заряжающийся ноутбук на полотенце может перегреться и воспламениться.';
+        document.querySelector('#tooltip3').html = '<strong>Опасность!</strong> Перегретые или поврежденные зарядные устройства могут вызвать пожар.';
+    } else {
+        document.querySelector('#tooltip1').html = '<strong>Опасность!</strong> Открытое окно может вызвать движение воздуха, которое может свалить горящую свечу и привести к пожару. Будьте осторожны с открытым огнем рядом с окнами.';
+        document.querySelector('#tooltip2').html = '<strong>Опасность!</strong> Заряжающийся ноутбук на полотенце может перегреться и воспламениться. Используйте твердые поверхности для зарядки устройств.';
+        document.querySelector('#tooltip3').html = '<strong>Опасность!</strong> Перегретые или поврежденные зарядные устройства могут вызвать пожар. Убедитесь, что провода не повреждены и не перегреваются.';
+    }
+}
+
+window.addEventListener('resize', changeText);
+window.addEventListener('load', changeText);
