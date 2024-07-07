077453f67c4901e34deae57f692e279e62530b8e
iwan <=>
2024-07-08 00:59:34 +0300

    Адаптив

--- fire_hazard/script.js
+++ fire_hazard/script.js
@@ -10,13 +10,13 @@ function showTooltip(event, id) {
 
 function changeText() {
     if (window.innerWidth <= 450) {
-        document.querySelector('#tooltip1').html = '<strong>Опасность!</strong> Открытое окно может опрокинуть горящую свечу и вызвать пожар.';
-        document.querySelector('#tooltip2').html = '<strong>Опасность!</strong> Заряжающийся ноутбук на полотенце может перегреться и воспламениться.';
-        document.querySelector('#tooltip3').html = '<strong>Опасность!</strong> Перегретые или поврежденные зарядные устройства могут вызвать пожар.';
+        document.getElementById('tooltip1').innerHTML = '<strong>Опасность!</strong> Открытое окно может опрокинуть горящую свечу и вызвать пожар.';
+        document.getElementById('tooltip2').innerHTML = '<strong>Опасность!</strong> Заряжающийся ноутбук на полотенце может перегреться и воспламениться.';
+        document.getElementById('tooltip3').innerHTML = '<strong>Опасность!</strong> Перегретые или поврежденные зарядные устройства могут вызвать пожар.';
     } else {
-        document.querySelector('#tooltip1').html = '<strong>Опасность!</strong> Открытое окно может вызвать движение воздуха, которое может свалить горящую свечу и привести к пожару. Будьте осторожны с открытым огнем рядом с окнами.';
-        document.querySelector('#tooltip2').html = '<strong>Опасность!</strong> Заряжающийся ноутбук на полотенце может перегреться и воспламениться. Используйте твердые поверхности для зарядки устройств.';
-        document.querySelector('#tooltip3').html = '<strong>Опасность!</strong> Перегретые или поврежденные зарядные устройства могут вызвать пожар. Убедитесь, что провода не повреждены и не перегреваются.';
+        document.getElementById('tooltip1').innerHTML = '<strong>Опасность!</strong> Открытое окно может вызвать движение воздуха, которое может свалить горящую свечу и привести к пожару. Будьте осторожны с открытым огнем рядом с окнами.';
+        document.getElementById('tooltip2').innerHTML = '<strong>Опасность!</strong> Заряжающийся ноутбук на полотенце может перегреться и воспламениться. Используйте твердые поверхности для зарядки устройств.';
+        document.getElementById('tooltip3').innerHTML = '<strong>Опасность!</strong> Перегретые или поврежденные зарядные устройства могут вызвать пожар. Убедитесь, что провода не повреждены и не перегреваются.';
     }
 }
 
